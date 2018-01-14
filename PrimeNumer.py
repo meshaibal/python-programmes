5bdd828b377f7b4428c5d7d9c1247114339c16c1
Shaibal <me.shaibal@gmail.com>
2018-01-14 20:01:54 +0530

    Used funtion to take input

--- PrimeNumer.py
+++ PrimeNumer.py
@@ -1,13 +1,24 @@
-inputNumber = input("Please Enter A Number:")
-try:
-    inputNumber = int(inputNumber)
-except:
-    print("Not a valid Number.")
-    quit()
+
+def user_input():
+    global validInput
+    global inputNumber
+    inputNumber = input("Please Enter A Number:")
+    try:
+        inputNumber = int(inputNumber)
+        validInput = True
+    except:
+        print("Not a valid Number.")
+        validInput = False
+    return inputNumber
+
+validInput = False
+inputNumber = 0
+while not validInput:
+    user_input()
 
 if inputNumber > 1:
-    for number in range(2,inputNumber):
-        if (inputNumber%number) == 0:
+    for n in range(2,inputNumber):
+        if (inputNumber%n) == 0:
             print(inputNumber,"is not a prime number.")
             break
     else:
